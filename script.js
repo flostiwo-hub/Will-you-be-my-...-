3aec687a51615eb5ab3927f9ba945e3c456d5bb5
flostiwo-hub <flo.stiwo@gmail.com>
2026-02-05 19:38:40 +0100

    V-DAY 2026
    Valentines26

--- script.js
+++ script.js
@@ -56,14 +56,14 @@
 const messages = [
     "Are you sure?",
     "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
+    "Are you REALLY REALLY sure?",
+    "Gaeeeee...",
+    "Think about it!",
+    "Slap me...",
+    "really hard...",
+    "like really really really hard...",
     "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Just kidding, I won't stop asking!"
 ];
 
 let messageIndex = 0;
